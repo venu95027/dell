a73ae19e243fdbaef08e36da3169050f3b83ab19
Nandini.T <nandinireddy723@gmail.com>
2017-12-10 12:58:51 +0530

    changed

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.addAttribute("message", "Welcome");
+		model.addAttribute("message1", "Welcome1");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
